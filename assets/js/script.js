79a686b7cbb8bb21cd3d36bf96beab6c6d2d8091
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 10:20:32 +0200

    ex4 partie2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-//fonction qui fait disparaitre les 3 blocs
-//fonction 2 qui fait au scroll sur la page recupére la valeur du scroll en vertical en px
+//déclaration fonction pageload qui fait disparaitre les 3 blocs au chargemnt
+window.onload = function pageload() {
+document.getElementById('blockLine').style.visibility = 'hidden';
+}
+//déclaration fonction onscroll qui fait disparaitre les blocs de la fenetre
+window.onscroll = function onScroll() {
+//recupération de la valeur du scroll vertical en nbre de px
+  var scrollTopValue = window.scrollY;
+//condition permettant de faire réapparaitre les blocs qd on scroll à 300
+  consol.log (scrollTopValue);
+  if (scrollTopValue > 300) {
+    document.getElementById('blockline').style.visibility = 'visible';
+  }
+}
 // et faire apparatire nos blocs
